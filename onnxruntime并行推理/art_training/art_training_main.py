ac0eb5aba2a6219d21bad08e8d3f1acad21da6d3
shixiuhai <64567345+shixiuhai@users.noreply.github.com>
2023-04-27 16:23:45 +0800

    Update art_training_main.py
    更新了调用模型的代码

--- onnxruntime并行推理/art_training/art_training_main.py
+++ onnxruntime并行推理/art_training/art_training_main.py
@@ -3,7 +3,6 @@ import cv2
 import numpy as np
 import base64
 from PIL import Image
-import threading
 import json
 # yolov5导出模型
 # python export.py --weights path/to/weights.pt --img 640 --batch 1
@@ -25,7 +24,9 @@ import subprocess
 # 创建一个5个线程的消费
 from concurrent.futures import ThreadPoolExecutor
 import queue
-from queue import Queue
+import time
+# from queue import Queue
+from multiprocessing import Queue,Process
 class ThreadPoolExecutor(ThreadPoolExecutor):
     def __init__(self, max_workers=None, thread_name_prefix=''):
         super().__init__(max_workers, thread_name_prefix)
@@ -53,121 +54,20 @@ def screenshot(videoPath:str)->np.ndarray:
         saveCmd = "ffmpeg -loglevel quiet -rtsp_transport tcp  -i %s -vframes 1 -f image2 -"%videoPath
     else:
         return np.array([])
-    p=subprocess.Popen(saveCmd.split(),stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = p.communicate(timeout=6)
-    if str(error)=="b''":
-        return cv2.imdecode(np.frombuffer(output, dtype=np.uint8), cv2.IMREAD_COLOR)
-    else:
+    try:
+        p = subprocess.Popen(saveCmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = p.communicate(timeout=6)
+        if output:
+            p.kill()
+            return cv2.imdecode(np.frombuffer(output, dtype=np.uint8), cv2.IMREAD_COLOR)
+        else:
+            p.kill()
+            return np.array([])
+    except Exception as error:
+        log.error("截图错误是%s"%error)
+        p.kill()
         return np.array([])
-
-
     
-# 添加新模型需要添加
-class ArtTraining:
-    """
-    在这里添加多个模型
-    """
-    def __init__(self,queueSize=10) -> None:
-        # 加载人头模型
-        self.headModel=Detector(640,0.6,0.6,"model/head.onnx",nameList=["head"])
-        # 创建人头模型生产者
-        self.headProducer=RabbitMqProducer(user=mqUser, pwd=mqPassword, host=mqHost, port=mqConnectPort,virtual_host=mqVirtualHost,exchange=mqExchange)
-        # 创建人头模型队列
-        self.headQueue=Queue(maxsize=queueSize)
-        print("-----------人头检测模型加载成功-----------")
-        
-        # 加载吸烟模型
-        self.smokeModel=Detector(640,0.6,0.6,"model/zju-smokingFace.onnx",nameList=["smoke"])
-        # 创建吸烟模型生成者
-        self.smokeProducer=RabbitMqProducer(user=mqUser, pwd=mqPassword, host=mqHost, port=mqConnectPort,virtual_host=mqVirtualHost,exchange=mqExchange)
-        # 创建吸烟模型生产者
-        self.smokeQueue=Queue(maxsize=queueSize)
-        print("-----------吸烟检测模型加载成功-----------")
-    
-    # 人头检测
-    def headDetect(self):
-        while True:
-            # print(self.headQueue.qsize())
-            item=self.headQueue.get()
-            id=item["id"]
-            checkType=item["checkType"]
-            imgArray=item["imgArray"]
-            if imgArray.size!=0:
-                # 这里要重写
-                res=self.headModel.detect_out(imgArray)
-                print("人头数量%s"%len(res))
-                # 这里要重写
-                self.headProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
-                    'id':id,
-                    'img':cv2img_to_base64(imgArray),
-                    'checkType':checkType,
-                    # 'objNumber':len(res),
-                    'data':str(res)
-                })
-                # for i in range(len(res)):
-                #         name = res[i]['name']
-                #         confidence_result = res[i]['confidence']
-                #         xmin = res[i]['xmin']
-                #         ymin = res[i]['ymin']
-                #         xmax = res[i]['xmax']
-                #         ymax = res[i]['ymax']
-                #         # 设置判断置信度对比和满足条件的type
-                #         if confidence_result>=0.2:
-                #             # print(name)
-                #             # 转换为x,y,width,height
-                #             x = int(xmin)
-                #             y =  int(ymin)
-                #             w =  int(xmax-xmin+1)
-                #             h =  int(ymax-ymin+1)
-                #             # 画框
-                #             # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness=3)
-                #             cv2.rectangle(imgArray, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
-                #             # 添加概率值
-                #             # cv2.putText(frame,"%s %.2s%%"%(name,confidence_result*100),(x+w+5,y-5),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-                #             cv2.putText(imgArray,"%s %.2s%%"%(name,confidence_result*100),(x+w+5,y-5),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,0,255),2)
-                # cv2.imwrite("10.png",imgArray)
-                # break
-            else:
-                # 这里要重写
-                self.headProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
-                    'id':id,
-                    'img':'',
-                    'checkType':checkType,
-                    # 'objNumber':len(res),
-                    'data':str(res)
-                })
-            
-    # 吸烟检测
-    def smokeDetect(self):
-        while True:
-            # print(self.smokeQueue.qsize())
-            item=self.smokeQueue.get()
-            id=item["id"]
-            checkType=item["checkType"]
-            imgArray=item["imgArray"]
-            # return self.headModel.detect_out(image=imgArray)
-            if imgArray.size!=0:
-                # 这里要重写
-                res=self.smokeModel.detect_out(imgArray)
-                # print(res)
-                # 这里要重写
-                self.smokeProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
-                    'id':id,
-                    'img':cv2img_to_base64(imgArray),
-                    'checkType':checkType,
-                    # 'objNumber':len(res),
-                    'data':str(res)
-                })
-            else:
-                # 这里要重写
-                self.smokeProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
-                    'id':id,
-                    'img':'',
-                    'checkType':checkType,
-                    # 'objNumber':len(res),
-                    'data':str(res)
-                })
-           
 
 # 添加新模型需要添加
 # 进行多线程截图
@@ -180,22 +80,209 @@ def screenshot_threads(id:str,path:str,checkType:str):
     """
     imgArray=screenshot(path)
     if "head" in checkType:
-        cv2.imwrite("source.png",imgArray)
-        artTraining.headQueue.put({
+        # print("添加人头成功")
+        headQueue.put({
             "id":id,
-            "checkType":checkType,
+            "checkType":"head",
             "imgArray":imgArray
         })
-        # print("人头添加图片成功")
+        # headQueue.get()
     
     if "smoke" in checkType:
-        artTraining.smokeQueue.put({
+        # print("添加吸烟目标成功")
+        smokeQueue.put({
             "id":id,
-            "checkType":checkType,
+            "checkType":"smoke",
             "imgArray":imgArray
         })
-        # print("添加吸烟图片成功")
+        # smokeQueue.get()
+    
+    if "fire" in checkType:
+        fireQueue.put({
+            "id":id,
+            "checkType":"fire",
+            "imgArray":imgArray
+        })
 
+# 添加新模型需要添加
+class ArtTrainingProcess:
+    """
+    在这里添加多个模型
+    """
+    def __init__(self) -> None:
+        print("---------开始启动多模型检测---------")
+    # 人头检测
+    @staticmethod
+    def headDetect():
+        headModel=Detector(640,0.6,0.6,"model/head.onnx",nameList=["head"])
+        headProducer=RabbitMqProducer(user=mqUser, pwd=mqPassword, host=mqHost, port=mqConnectPort,virtual_host=mqVirtualHost,exchange=mqExchange)
+        count=0
+        countMax=100
+        print("---------艺术培训人头检测模型启动成功---------")
+        log.info("---------艺术培训人头检测模型启动成功---------")
+        while True:
+            if count>countMax:
+                count=0
+                print("人头检测队列大小"+str(headQueue.qsize()))
+                log.info("人头检测队列大小"+str(headQueue.qsize()))
+            try:
+                if headQueue.qsize()>=1:
+                    count=count+1
+                    item=headQueue.get()
+                    id=item["id"]
+                    checkType=item["checkType"]
+                    imgArray=item["imgArray"]
+                    if imgArray.size!=0:
+                        # print("人头检测成功")
+                        # 这里要重写
+                        res=headModel.detect_out(imgArray)
+                        # print("--------------人头检测成功")
+                        if res!=[]:
+                            print("检测到人头数量是%s"%len(res))
+                            # 这里要重写
+                            headProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':str(res),
+                                'img':cv2img_to_base64(imgArray)
+                            })
+                        else:
+                            # 这里要重写
+                            headProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':str(res),
+                                'img':''
+                            })
+                    else:
+                        headProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':"[]",
+                                'img':''
+                            })
+                else:
+                    # 队列目标数量小于某个量级后进行等待队列添加
+                    time.sleep(0.1)
+            except Exception as error:
+                log.error("代码167行,人头检测出现错误,错误是%s"%error)
+                
+    # 吸烟检测
+    @staticmethod
+    def smokeDetect():
+        smokeModel=Detector(640,0.6,0.6,"model/zju-smokingFace.onnx",nameList=["smoke"])
+        smokeProducer=RabbitMqProducer(user=mqUser, pwd=mqPassword, host=mqHost, port=mqConnectPort,virtual_host=mqVirtualHost,exchange=mqExchange)
+        count=0
+        countMax=100
+        print("---------场所吸烟检测模型启动成功---------")
+        log.info("---------场所吸烟检测模型启动成功---------")
+        while True:
+            if count>countMax:
+                count=0
+                print("吸烟检测队列大小"+str(smokeQueue.qsize()))
+                log.info("吸烟检测队列大小"+str(smokeQueue.qsize()))
+            try:
+                if smokeQueue.qsize()>=1:
+                    count=count+1
+                    item=smokeQueue.get()
+                    id=item["id"]
+                    checkType=item["checkType"]
+                    imgArray=item["imgArray"]
+                    # 成功截图
+                    if imgArray.size!=0:
+                        # print("吸烟检测成功")
+                        # 这里要重写
+                        res=smokeModel.detect_out(imgArray)
+                        if res!=[]:
+                            # 这里要重写
+                            smokeProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':str(res),
+                                'img':cv2img_to_base64(imgArray)
+                            })
+                        else:
+                            # 这里要重写
+                            smokeProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':str(res),
+                                'img':''
+                            })
+                    # 没有成功截图
+                    else:
+                        smokeProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':"[]",
+                                'img':''
+                            })
+                else:
+                    # 队列目标数量小于某个量级后进行等待队列添加
+                    time.sleep(0.1)
+            
+            except Exception as error:
+                log.error("代码226行,吸烟检测出现错误,错误是%s"%error)
+
+    # 火焰检测
+    @staticmethod
+    def fireDetect():
+        fireModel=Detector(640,0.6,0.6,"model/fire.onnx",nameList=["fire"])
+        fireProducer=RabbitMqProducer(user=mqUser, pwd=mqPassword, host=mqHost, port=mqConnectPort,virtual_host=mqVirtualHost,exchange=mqExchange)
+        count=0
+        countMax=100
+        print("---------场所火焰检测模型启动成功---------")
+        log.info("---------场所火焰检测模型启动成功---------")
+        while True:
+            if count>countMax:
+                count=0
+                print("火焰检测队列大小"+str(fireQueue.qsize()))
+                log.info("火焰检测队列大小"+str(fireQueue.qsize()))
+            try:
+                if fireQueue.qsize()>=1:
+                    count=count+1
+                    item=fireQueue.get()
+                    id=item["id"]
+                    checkType=item["checkType"]
+                    imgArray=item["imgArray"]
+                    # 成功截图
+                    if imgArray.size!=0:
+                        # print("吸烟检测成功")
+                        # 这里要重写
+                        res=fireModel.detect_out(imgArray)
+                        # print("+++++++++吸烟检测成功")
+                        if res!=[]:
+                            # 这里要重写
+                            fireProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':str(res),
+                                'img':cv2img_to_base64(imgArray)
+                            })
+                        else:
+                            # 这里要重写
+                            fireProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':str(res),
+                                'img':''
+                            })
+                    # 没有成功截图
+                    else:
+                        fireProducer.send(queueName=monitoringWarningQueueName,routingKey=monitoringWarningKey,body={
+                                'id':id,
+                                'checkType':checkType,
+                                'data':"[]",
+                                'img':''
+                            })
+                else:
+                    # 队列目标数量小于某个量级后进行等待队列添加
+                    time.sleep(0.1)
+            
+            except Exception as error:
+                print(res)
+                log.error("代码285行,火焰检测出现错误,错误是%s"%error)
+    
     
 def callback(ch, method, properties, body):
         # 接收消息
@@ -209,13 +296,10 @@ def callback(ch, method, properties, body):
             #detect(id=id,path=path,checkType=checkType)
             # 手动确认消费
             ch.basic_ack(delivery_tag=method.delivery_tag)
-            # Thread(target=process_video_to_images,args=(path,save_path,companyId,taskId,anchorId,)).start()
-            # MyThread(process_video_to_images,args=(path,save_path,companyId,taskId,anchorId,)).start()
         except Exception as error:
             log.error("callbak错误是：%s"%error)
         finally:
             return
-        
 
 if __name__ == "__main__":
     """
@@ -225,26 +309,33 @@ if __name__ == "__main__":
     定义日志生产者
     """
     # 创建一个log对象
-    log=Log(__name__, 'art_training_main.log').Logger
+    log=Log(__name__, 'model_training_main.log').Logger
     log_timeInterval=Log_timeInterval("log2", '/home/wenhua/python/log/record.log').Logger
     
     # 创建一个进行截图的线程池
-    threadPool = ThreadPoolExecutor(max_workers=15) 
+    threadPool = ThreadPoolExecutor(max_workers=36) 
     
     # 添加新模型需要添加
-    # 定义一个检测类
-    artTraining=ArtTraining()
-    # 启动人头检测
-    threading.Thread(target=artTraining.headDetect).start()
-    # 启动吸烟检测
-    threading.Thread(target=artTraining.smokeDetect).start()
+    # 创建一个人头队列
+    headQueue=Queue(maxsize=10)
+    # 创建一个吸烟队列
+    smokeQueue=Queue(maxsize=10)
+    # 创建一个火焰检测队列
+    fireQueue=Queue(maxsize=10)
     
-    #image=imread_fast("2.png",channel_order='rgb')
-    # image=cv2.imread("2.png")
-    # print(artTraining.headDetect(image=image))
+    # 启动模型检测
+    Process(target=ArtTrainingProcess.headDetect).start()
+    time.sleep(2)
+    # Process(target=ArtTrainingProcess.headDetect).start()
+    Process(target=ArtTrainingProcess.smokeDetect).start()
+    time.sleep(2)
+    # Process(target=ArtTrainingProcess.smokeDetect).start()
+    Process(target=ArtTrainingProcess.fireDetect).start()
+    # Process(target=ArtTrainingProcess.fireDetect).start()
+
+    
+    # 启动任务消费
     RabbitMqConsumer(user=mqUser, pwd=mqPassword, host=mqHost, port=mqConnectPort,virtual_host=mqVirtualHost,exchange=mqExchange).receive(queueName=detectPhotoQueueName,routingKey=detectPhotoQueueKey,fun=callback)
-    
-    
     
     
     
